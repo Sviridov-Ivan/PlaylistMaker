83e6fc480f5d7b8681bc4c2e6ada1141f3b1b0a5
Sviridov-Ivan <sviridov_ivan@mail.ru>
2025-05-12 16:30:29 +0500

    Creating Commit#3

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,13 +1,55 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.test_lord_of_the_rings)
+        setContentView(R.layout.activity_main)
+
+        //Взаимодействие с кнопкой ПОИСК через реализацию анонимного класса
+        val buttonSearch = findViewById<Button>(R.id.button_search)
+
+        val buttonSearchClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на кнопку Поиск!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        buttonSearch.setOnClickListener(buttonSearchClickListener)
+
+        //Взаимодействие с кнопкой МЕДИАТЕКА через реализацию анонимного класса
+        val buttonMedia = findViewById<Button>(R.id.button_media)
+
+        val buttonMediaClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на кнопку Медиатека!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        buttonMedia.setOnClickListener(buttonMediaClickListener)
+
+
+        val buttonSettings = findViewById<Button>(R.id.button_settings)
+
+        val buttonSettingsClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на кнопку Настройки!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        buttonSettings.setOnClickListener(buttonSettingsClickListener)
+
+
 
     }
 }
+
+
+
+
