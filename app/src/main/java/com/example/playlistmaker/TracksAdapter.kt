aad403f0938e98f39d5cb34b4a6792f509148635
Sviridov-Ivan <sviridov_ivan@mail.ru>
2025-06-29 15:49:40 +0500

    Creating Commit#13 Спринт №11

--- app/src/main/java/com/example/playlistmaker/TracksAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/TracksAdapter.kt
@@ -4,9 +4,10 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class TracksAdapter(
-    private val tracks: List<Track>
-) : RecyclerView.Adapter<TracksViewHolder> () {
+class TracksAdapter() : RecyclerView.Adapter<TracksViewHolder> () { // убрал private val tracks: List<Track> из конструктора (было нужно для заглушки 10 спринта)
+
+    var tracks = ArrayList<Track>()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TracksViewHolder {
         // val view = LayoutInflater.from(parent.context).inflate(R.layout.activity_track, parent, false) // традиционно inflate размещается здесь, но так необходимо по заданию
         return TracksViewHolder(parent) // здесь вместо parent было view при реализации inflate в строке выше
